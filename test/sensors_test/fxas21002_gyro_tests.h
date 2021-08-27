c098a65f0acf90de3b01b2dd20a0069971e14779
michaelwro <michael.wrona77@gmail.com>
2021-08-27 13:59:55 -0500

    got rid of extra lines

--- test/sensors_test/fxas21002_gyro_tests.h
+++ test/sensors_test/fxas21002_gyro_tests.h
@@ -19,9 +19,6 @@
 #include "hummingbird_config.h"
 #include "sensor_drivers/fxas21002_gyro.h"
 
-/* Global gyro object for testing */
-// FXAS21002Gyro gyro(&SENSOR_I2C);
-
 void test_fxas21002_init_250dps(void);
 void test_fxas21002_init_500dps(void);
 void test_fxas21002_init_1000dps(void);
@@ -31,9 +28,5 @@ void test_fxas21002_self_test(void);
 
 void FXAS21002_I2Cwrite8(TwoWire *sensorWire, uint8_t regOfInterest, uint8_t valToWrite);
 uint8_t FXAS21002_I2Cread8(TwoWire *sensorWire, uint8_t regOfInterest);
-
-
-
-
 
 #endif  // UNIT_TEST
